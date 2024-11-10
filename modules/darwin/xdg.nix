eea300c7b529c026a1fcd7e79ab1bf4f7f4de4dd
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2024-11-10 20:23:11 +0100

    add darwin xdg module

---
+++ modules/darwin/xdg.nix
@@ -0,0 +1,10 @@
+{
+  environment = {
+    variables = {
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_BIN_HOME = "$HOME/.local/bin";
+    };
+  };
+}
