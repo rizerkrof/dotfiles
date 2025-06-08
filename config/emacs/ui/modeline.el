0adab4c5a14a6c0172cdf91d033468bfbdb537d9
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2025-06-08 11:59:30 +0200

    disable nyan mode when window split

--- config/emacs/ui/modeline.el
+++ config/emacs/ui/modeline.el
@@ -8,4 +8,21 @@
 
 (use-package
  nyan-mode
- :config (nyan-mode) (nyan-start-animation) (nyan-toggle-wavy-trail))
+ :config
+ (defun activate-nyan-mode ()
+   (nyan-mode)
+   (nyan-start-animation)
+   (nyan-toggle-wavy-trail))
+
+ (defun toggle-nyan-for-windows ()
+   (if (> (length (window-list)) 1)
+       (nyan-mode -1)
+     (activate-nyan-mode)))
+
+ (activate-nyan-mode)
+
+ (add-hook
+  'window-configuration-change-hook #'toggle-nyan-for-windows)
+ (add-hook
+  'window-size-change-functions
+  (lambda (frame) (toggle-nyan-for-windows))))
