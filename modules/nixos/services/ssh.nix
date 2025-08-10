20f1580cfb784b89ab0ddf751ed3562db7fc9a7d
rizerkrof <34008166+rizerkDrof@users.noreply.github.com>
2025-08-10 15:09:54 +0200

    add ssh service

---
+++ modules/nixos/services/ssh.nix
@@ -0,0 +1,41 @@
+{ lib, config, options, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.ssh;
+in {
+  options.modules.services.ssh = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.openssh = {
+      enable = true;
+      
+      settings = {
+        KbdInteractiveAuthentication = false;
+        # Require keys over passwords. Ensure target machines are provisioned
+        # with authorizedKeys!
+        PasswordAuthentication = false;
+      };
+
+      # Deactivate short moduli
+      moduliFile = pkgs.runCommand "filterModuliFile" {} ''
+        awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
+      '';
+
+      # Removes the default RSA key (not that it represents a vulnerability, per
+      # se, but is one less key (that I don't plan to use) to the castle laying
+      # around) and improves the ed25519 key's entropy by generating it with 100
+      # rounds (default is 16).
+      hostKeys = [
+        {
+          comment = "${config.networking.hostName}.local";
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          rounds = 100;
+          type = "ed25519";
+        }
+      ];
+    };
+  };
+}
