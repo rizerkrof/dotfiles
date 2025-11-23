3ef5cb24e0ce37a860ee5f6e43811300dcc133bc
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2025-11-23 14:55:03 +0100

    fix line flick due to box prop for eglot current symbol

--- config/emacs/dev/lsp.el
+++ config/emacs/dev/lsp.el
@@ -5,4 +5,6 @@
  (add-hook 'tsx-ts-mode-hook 'eglot-ensure)
  (add-hook 'js-ts-mode-hook 'eglot-ensure))
 
-(custom-set-faces '(eglot-highlight-symbol-face ((t (:box "red")))))
+(custom-set-faces
+ '(eglot-highlight-symbol-face ((t (:box (:line-width (-1 . -1) :color "red") :inherit default)))))
+
